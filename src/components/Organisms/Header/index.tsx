a2e49ef70ecf85cf9087eeee983baa8d07f5a7c8
Xristos-Marios Dimopoulos <chdimop@gmail.com>
2024-06-23 19:08:46 +0300

    started making header

--- src/components/Organisms/Header/index.tsx
+++ src/components/Organisms/Header/index.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header className="sticky top-5 max-w-[1024px] w-full p-4 bg-[#18181D]/30 flex items-center justify-between mx-auto">
+    <header className="sticky top-5 max-w-[1024px] w-full p-4 bg-[#18181D]/30 flex items-center justify-between mx-auto rounded-[12px] backdrop-blur-sm">
       <div className="flex items-center">
         <Link href="/">
           <LogoIcon className="w-[100px] h-[30px] text-white" />
