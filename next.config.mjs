2f0a5df13bcbc2b1c345b9386c991b4969309519
Xristos-Marios Dimopoulos <chdimop@gmail.com>
2024-07-21 00:48:36 +0300

    fix nodemailer

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ import { withPayload } from '@payloadcms/next/withPayload'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your Next.js config here
+  output: 'standalone',
   experimental: {
     reactCompiler: false
   },
